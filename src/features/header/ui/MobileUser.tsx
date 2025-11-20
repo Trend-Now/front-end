ff19a6db0f83fa5401ca60e9a34c71a61698fa11
GuNyangE <noel7145@gmail.com>
2025-11-21 01:49:19 +0900

    feature: 유저 프로필 아이콘 작업중

--- src/features/header/ui/MobileUser.tsx
+++ src/features/header/ui/MobileUser.tsx
@@ -2,82 +2,74 @@
 
 // import { axiosUserProfile } from '@/shared/api';
 // import { UserProfile } from '@/shared/types';
-import { Search24, Hamburger24, PrimaryButton } from '@/shared/ui';
+import { Search24, Hamburger24, DropdownMenu, DropdownMenuItem } from '@/shared/ui';
 // import { useQuery } from '@tanstack/react-query';
-import React, { useState } from 'react';
-import Close from './icon/Close';
-import SignOut from './icon/SignOut';
+import React from 'react';
+import { logoutAction } from '@/features/logout';
+import { useQueryClient } from '@tanstack/react-query';
+import { useRouter } from 'next/navigation';
+import UserIcon from './icon/UserIcon';
 
 export default function MobileUser() {
-  const [sideBarOpen, setSidebarOpen] = useState(false);
+  const queryClient = useQueryClient();
+  const router = useRouter();
 
   // const { data: user, isError } = useQuery<UserProfile | null>({
   //   queryKey: ['userInfo'],
   //   queryFn: () => axiosUserProfile(),
   // });
-
-  const handleSideBarOpen = () => {
-    setSidebarOpen(true);
-    document.body.style.overflow = 'hidden';
-  };
-
-  const handleSideBarClose = () => {
-    setSidebarOpen(false);
-    document.body.style.overflow = '';
-  };
+  const menus = [
+    { name: '내가 작성한 게시글', path: '/mypage/posts' },
+    { name: '내가 작성한 댓글', path: '/mypage/comments' },
+    { name: '스크랩한 게시글', path: '/mypage/scraps' },
+    { name: '설정', path: '/mypage/settings' },
+  ];
 
   return (
     <span className="flex items-center gap-2 md:hidden">
       <button className="flex h-9 w-9 items-center justify-center rounded-full bg-gray-100">
         <Search24 className="fill-gray-900" />
       </button>
-      <button
-        onClick={handleSideBarOpen}
-        className="flex h-9 w-9 items-center justify-center rounded-full bg-gray-100"
+      {/* 로그아웃인 상태에서는 "로그인" 문구 버튼 */}
+      <DropdownMenu
+        trigger={
+          <span className="flex h-9 w-9 items-center justify-center rounded-full bg-gray-100">
+            <Hamburger24 />
+          </span>
+        }
+        className="w-fit gap-y-2 p-4"
       >
-        <Hamburger24 />
-      </button>
-      <input type="checkbox" readOnly checked={sideBarOpen} className="peer absolute h-0 w-0" />
-      <div
-        onClick={(e) => {
-          if (e.target === e.currentTarget) {
-            handleSideBarClose();
-          }
-        }}
-        className="pointer-events-none fixed inset-0 z-40 h-full w-full bg-gray-900/30 opacity-0 transition-opacity duration-200 ease-out peer-checked:pointer-events-auto peer-checked:opacity-100"
-      />
-      <span className="fixed bottom-14 right-0 top-14 z-50 flex w-72 translate-x-full flex-col gap-y-2 rounded-l-3xl border bg-white p-4 transition-transform peer-checked:-translate-x-0">
-        <div className="flex justify-end py-1.5">
-          <button onClick={handleSideBarClose}>
-            <Close />
-          </button>
-        </div>
+        <span className="flex h-10 w-fit items-center gap-x-2 rounded-full bg-gray-100 pl-2.5 pr-3.5">
+          <UserIcon />
+          <span className="text-nowrap text-md font-medium text-gray-800">Trendnow</span>
+        </span>
         <hr className="h-px border-0 bg-gray-200" />
-        <div className="flex h-full flex-col justify-between">
-          <div className="flex flex-col gap-y-1">
-            <MenuRow text="내가 작성한 게시글" />
-            <MenuRow text="내가 작성한 댓글" />
-            <MenuRow text="스크랩한 게시글" />
-            <MenuRow text="설정" />
-          </div>
-          <PrimaryButton variant="gray" size="m" className="h-10 w-fit gap-x-1.5 rounded-full p-3">
-            <SignOut />
+        <div className="flex flex-col gap-y-1">
+          {menus.map((menu) => (
+            <DropdownMenuItem
+              key={menu.name}
+              onClick={() => router.push(menu.path)}
+              className="h-10 text-sm"
+            >
+              {menu.name}
+            </DropdownMenuItem>
+          ))}
+          <DropdownMenuItem
+            onClick={async () => {
+              const confirmSignout = confirm('정말 로그아웃 하실 건가요?');
+
+              if (confirmSignout) {
+                await logoutAction();
+                queryClient.clear();
+                router.refresh();
+              }
+            }}
+            className="h-10 text-sm"
+          >
             로그아웃
-          </PrimaryButton>
+          </DropdownMenuItem>
         </div>
-      </span>
+      </DropdownMenu>
     </span>
-  );
-}
-
-interface MenuRowProps {
-  text: string;
-}
-
-function MenuRow({ text }: MenuRowProps) {
-  return (
-    <div className="flex h-10 cursor-pointer items-center rounded-xl text-sm font-medium text-gray-800 hover:bg-gray-100">
-      {text}
-    </div>
   );
 }
