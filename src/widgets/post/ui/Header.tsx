9dec939b490148409f2a8efc83bd1c666193c422
kangsuyeong <ekdh16@naver.com>
2025-12-06 15:53:34 +0900

    feat: 반응형 상세구현

--- src/widgets/post/ui/Header.tsx
+++ src/widgets/post/ui/Header.tsx
@@ -21,12 +21,35 @@ interface HeaderProps {
 
 export default function Header({ post, isHotBoard, boardId, postId }: HeaderProps) {
   return (
-    <div className="flex flex-col gap-y-6 border-b border-gray-200 pb-5">
-      <div className="flex justify-between">
+    <div
+      className={cn(
+        'flex flex-col border-b border-gray-200',
+        'gap-y-4 pb-4', // 기본 스타일 (모바일)
+        'md:gap-y-6 md:pb-5' // 태블릿 이상 스타일
+      )}
+    >
+      <div
+        className={cn(
+          'flex',
+          'flex-col-reverse justify-normal gap-y-4', // 기본 스타일 (모바일)
+          'md:gay-y-0 md:flex-row md:justify-between' // 태블릿 이상 스타일
+        )}
+      >
         {/* 게시판 이름 & 제목 */}
-        <div className="flex flex-col gap-y-3 pr-10">
+        <div
+          className={cn(
+            'flex flex-col',
+            'gap-y-2', // 기본 스타일 (모바일)
+            'md:gap-y-3' // 태블릿 이상 스타일
+          )}
+        >
           <div
-            className={cn('text-lg font-semiBold', isHotBoard ? 'text-brand-500' : 'text-gray-500')}
+            className={cn(
+              'font-semiBold',
+              'text-sm', // 기본 스타일 (모바일)
+              'md:text-lg', // 태블릿 이상 스타일
+              isHotBoard ? 'text-brand-500' : 'text-gray-500'
+            )}
           >
             {post.boardName}
           </div>
@@ -40,29 +63,56 @@ export default function Header({ post, isHotBoard, boardId, postId }: HeaderProp
         </div>
       </div>
 
-      <div className="flex justify-between">
-        <span className="flex items-center gap-x-2">
-          <UserIcon className="h-7 w-7" maskId="post_icon" />
-          <span className="text-base font-medium text-gray-500">{post.writer}</span>
-        </span>
-        <span className="flex items-center text-sm font-regular text-gray-500">
-          <span className="flex items-center gap-x-1.5 rounded-md bg-gray-100 px-2.5 py-1">
+      <div
+        className={cn(
+          'flex justify-between',
+          'flex-col gap-y-2', // 기본 스타일 (모바일)
+          'md:flex-row md:gap-y-0' // 태블릿 이상 스타일
+        )}
+      >
+        <div className="flex items-center gap-x-2">
+          <UserIcon
+            className={cn(
+              'h-6 w-6', // 기본 스타일 (모바일)
+              'md:h-7 md:w-7' // 태블릿 이상 스타일
+            )}
+            maskId="post_icon"
+          />
+          <span
+            className={cn(
+              'font-medium text-gray-500',
+              'text-sm', // 기본 스타일 (모바일)
+              'md:text-base' // 태블릿 이상 스타일
+            )}
+          >
+            {post.writer}
+          </span>
+        </div>
+
+        <ul
+          className={cn(
+            'flex items-center whitespace-nowrap font-regular text-gray-500',
+            'text-2xs', // 기본 스타일 (모바일)
+            'md:text-sm' // 태블릿 이상 스타일
+          )}
+        >
+          <li className="flex items-center gap-x-1.5 rounded-md bg-gray-100 px-2.5 py-1">
             <span>댓글</span>
             <span>{post.commentCount.toLocaleString()}</span>
-          </span>
-          <span className="flex items-center gap-x-1.5 before:ml-2 before:inline-block before:h-3 before:w-[1px] before:bg-gray-200">
+          </li>
+          <li className="flex items-center gap-x-1.5 before:ml-2 before:inline-block before:h-3 before:w-[1px] before:bg-gray-200">
             <span>조회수</span>
             <span>{post.viewCount.toLocaleString()}</span>
-          </span>
-          <span className="flex items-center gap-x-1.5 before:ml-2 before:inline-block before:h-3 before:w-[1px] before:bg-gray-200">
+          </li>
+          <li className="flex items-center gap-x-1.5 before:ml-2 before:inline-block before:h-3 before:w-[1px] before:bg-gray-200">
             <span>추천</span>
             <span>{post.likeCount.toLocaleString()}</span>
-          </span>
-          <span className="flex items-center gap-x-1.5 before:ml-2 before:inline-block before:h-3 before:w-[1px] before:bg-gray-200">
+          </li>
+          <li className="flex items-center gap-x-1.5 before:ml-2 before:inline-block before:h-3 before:w-[1px] before:bg-gray-200">
             <span>작성일</span>
-            <span>{dayjs(post.createdAt).format('YYYY.MM.DD')}</span>
-          </span>
-        </span>
+            <time dateTime={post.createdAt}>{dayjs(post.createdAt).format('YYYY.MM.DD')}</time>
+          </li>
+        </ul>
       </div>
     </div>
   );
