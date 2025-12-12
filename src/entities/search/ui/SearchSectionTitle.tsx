a29343c1141640630c5c794cdb014dbf0d6c625c
kangsuyeong <ekdh16@naver.com>
2025-12-12 21:36:47 +0900

    feat: 실시간 인기 게시판 반응형 구현

--- src/entities/search/ui/SearchSectionTitle.tsx
+++ src/entities/search/ui/SearchSectionTitle.tsx
@@ -7,7 +7,7 @@ interface SearchSectionTitleProps {
 
 const SearchSectionTitle = ({ title, count }: SearchSectionTitleProps) => {
   return (
-    <div className={cn('flex gap-x-2 text-2xl font-bold')}>
+    <div className={cn('flex gap-x-2 text-base font-bold md:text-2xl')}>
       <span className="text-gray-800">{title}</span>
       <span className={cn(count === 0 ? 'text-gray-500' : 'text-brand-500')}>{count}건</span>
     </div>
